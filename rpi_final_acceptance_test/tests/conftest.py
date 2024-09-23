a8836cac6276e0a80c8b22d4bf5033432732b716
xanderhendriks <xander@nx-solutions.com>
2024-09-23 22:34:12 +1000

    Added target programming and implemneted real tests

--- rpi_final_acceptance_test/tests/conftest.py
+++ rpi_final_acceptance_test/tests/conftest.py
@@ -1,49 +1,60 @@
 import datetime
 import logging
-import os
+import serial
+import time
 import pytest
-import subprocess
-import yaml
+
+from threading import Thread
 
 
-file_logging = logging.getLogger()
+file_logging = logging.getLogger('main')
 
 
-# def program_stm32_flash(binary_file):
-#     command = [
-#         'openocd',
-#         '-f', 'interface/stlink.cfg',
-#         '-f', 'target/stm32f3x.cfg',
-#         '-c', f'program {binary_file} verify reset exit 0x8000000'
-#     ]
+def pytest_collection_modifyitems(session, config, items):
+    # Ensure that 'test_program_device.py' is executed first
+    program_device_tests = []
+    other_tests = []
 
-#     subprocess.check_output(command, stderr=subprocess.STDOUT)
+    for item in items:
+        if 'test_program.py' in item.nodeid:
+            program_device_tests.append(item)
+        else:
+            other_tests.append(item)
 
-
-# @pytest.fixture(scope="session", autouse=True)
-# def jig(jig_config):
-#     filename = f"sample_application-{jig_config['firmware']['version']}.bin"
-#     file_logging.info(f'Programming target with {filename}')
-#     program_stm32_flash(f"{os.path.dirname(os.path.realpath(__file__))}/../../files/firmware/{filename}")
-#     yield
-#     file_logging.info('teardown')
+    # Place 'test_program_device.py' items at the start
+    items[:] = program_device_tests + other_tests
 
 
-# @pytest.fixture(scope="session")
-# def jig_config(pytestconfig):
-#     with open(pytestconfig.getoption("--config"), 'r') as file:
-#         jig_config = yaml.safe_load(file)
-#     return jig_config
+class SerialCommandResponse:
+    def __init__(self, port):
+        self.thread = None
+        self.serial = serial.Serial(port, 9600, timeout=1)
+        self.serial.read(1000)
+
+    def command(self, command):
+        self.response_string = None
+        self.thread = Thread(target=self._process)
+        self.thread.start()
+        self.serial.write(command.encode())
+
+    def response(self, timeout=0.5):
+        start_time = time.time()
+        while self.response_string is None and time.time() < (start_time + timeout):
+            pass
+        time.sleep(1)
+        self.thread.join()
+        return self.response_string
+
+    def _process(self):
+        """
+        Process for reading the serial debug port
+        """
+        self.response_string = self.serial.readline().decode().rstrip()
 
 
-# @pytest.fixture(scope="session")
-# def version_to_check(jig_config):
-#     return jig_config['firmware']['version']
-
-
-# @pytest.fixture(scope="session")
-# def git_hash_to_check(jig_config):
-#     return jig_config['firmware']['git_hash']
+@pytest.fixture(scope="session")
+def serial_command_response(pytestconfig):
+    return SerialCommandResponse(pytestconfig.getoption('--st-link-com-port'))
 
 
 @pytest.fixture(scope="session")
@@ -56,12 +67,19 @@ def operator_name(pytestconfig):
     return pytestconfig.getoption("--operator-name")
 
 
-# @pytest.fixture(scope="function", autouse=True)
-# def log_test_start(request):
-#     file_logging.info('=' * 5 + request.node.name.ljust(70, '='))
-#     file_logging.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+@pytest.fixture(scope="session")
+def firmware_version(pytestconfig):
+    return pytestconfig.getoption("--firmware-version")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def log_test_start(request):
+    file_logging.info('=' * 5 + request.node.name.ljust(70, '='))
+    file_logging.info(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
 
 def pytest_addoption(parser):
-    parser.addoption("--board-serial-number", action="store", default="XXXXXXXXXX")
+    parser.addoption('--st-link-com-port', action='store')
+    parser.addoption("--board-serial-number", action="store")
     parser.addoption("--operator-name", action="store")
+    parser.addoption("--firmware-version", action="store")
--- rpi_final_acceptance_test/tests/test_sample_application.py
+++ rpi_final_acceptance_test/tests/test_sample_application.py
@@ -1,14 +1,50 @@
 import logging
 import time
-
-file_logging = logging.getLogger()
-
-
-def test_version():
-    a = 5
-    b = 10
-    assert a == b, f"Expected {a} to be equal to {b}, but they are not."
+import re
 
 
-def test_sensor():
-    pass
+def test_version(serial_command_response, firmware_version):
+    """
+    Check the version
+    """
+    pattern = r"image_id:\s*(\d+),\s*version:\s*(\d+\.\d+\.\d+)-([a-f0-9]+)"
+
+    serial_command_response.command('v')
+    response = serial_command_response.response()
+
+    print(f'response: {response}')
+
+    match = re.search(pattern, response)
+
+    assert match, "Can't read the image id and version"
+    assert match.group(1) == '1', 'Incorrect image ID'
+
+    if 'dev' in firmware_version:
+        assert match.group(2) == '0.0.0', 'Incorrect version'
+        assert match.group(3) == firmware_version.split('-')[0], 'Incorrect githash'
+    else:
+        assert match.group(2) == firmware_version, 'Incorrect version'
+
+
+def test_sensor(serial_command_response):
+    """
+    Test the sensor output
+    """
+    serial_command_response.command('s')
+    response = serial_command_response.response()
+
+    print(f'response: {response}')
+
+    assert response == 'sensor: 65535'
+
+
+def test_sensor_failing(serial_command_response):
+    """
+    Test the sensor output
+    """
+    serial_command_response.command('s')
+    response = serial_command_response.response()
+
+    print(f'response: {response}')
+
+    assert response == 'sensor: 0', 'Replace sensor'
